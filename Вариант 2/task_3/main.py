d1223376fe2c3a38307b2abbf0d2cb2cd55e3212
Даша <darakulkina138@gmail.com>
2025-10-14 16:25:43 +0300

    Update main.py

--- Вариант 2/task_3/main.py
+++ Вариант 2/task_3/main.py
@@ -1,4 +1,6 @@
 #Автор darakulkina138-netizen
-num = [i * 3 for i in range(1, 11)]
-print("Первые 10 чисел, умноженные на 3: ")
-print(num)
+num = int(input("Введите число: "))
+print(f"Нечетные числа от 1 до {num}: ")
+for i in range(1, num + 1):
+    if i % 2 != 0:
+        print(i)
