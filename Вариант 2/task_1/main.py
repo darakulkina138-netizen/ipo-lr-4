f6e621fa4850ccc41a9ca7f865260c392f5054a6
Даша <darakulkina138@gmail.com>
2025-10-14 16:19:03 +0300

    Update main.py

--- Вариант 2/task_1/main.py
+++ Вариант 2/task_1/main.py
@@ -1,1 +1,8 @@
-
+#Автор darakulkina138-netizen
+num = int(input("Введите число для расчета факториала: "))
+fact = 1
+i = 1
+while i <= num:
+    fact *= i 
+    i += 1
+print(f"Факториал числа {num} равен {fact}")
